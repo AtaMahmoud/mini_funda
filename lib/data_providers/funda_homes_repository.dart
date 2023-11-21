7677cf8d0c4b21115ad0a3c7bcfc58e6781d4d21
Ata Mahmoud <ata.ata250@hotmail.com>
2023-11-21 01:09:18 +0100

    Add funda homes repository

---
+++ lib/data_providers/funda_homes_repository.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+
+import '../models/house.dart';
+import '../models/house_details.dart';
+import 'funda_api_client.dart';
+
+abstract class HomeFetchFailure with EquatableMixin implements Exception {
+  const HomeFetchFailure(this.error);
+
+  final Object error;
+
+  @override
+  List<Object> get props => [error];
+}
+
+class GetHousesByCityFailure extends HomeFetchFailure {
+  const GetHousesByCityFailure(super.error);
+}
+
+class GetHouseByIdFailure extends HomeFetchFailure {
+  const GetHouseByIdFailure(super.error);
+}
+
+class FundaHomesRepository {
+  const FundaHomesRepository({required FundaApiClient apiClient})
+      : _apiClient = apiClient;
+
+  final FundaApiClient _apiClient;
+
+  Future<List<House>> getHousesBy(String city) async {
+    try {
+      return await _apiClient.getHomesBy(city);
+    } catch (e) {
+      throw GetHousesByCityFailure(e);
+    }
+  }
+
+  Future<HouseDetails> getHouseDetailsBy(String id) async {
+    try {
+      return await _apiClient.getHomeDetailsBy(id);
+    } catch (e) {
+      throw GetHouseByIdFailure(e);
+    }
+  }
+}
